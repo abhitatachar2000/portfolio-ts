5a477ce16af972a23fc285c48005a1d1d9ea6b7b
Abhishek Tatachar <abhishektatachar@macbook-air.local>
2025-02-15 13:09:13 +0530

    adapting the nav-bar to mobile views

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./NavBar.css"
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export function NavBar() {
+    const navBarSelectedItemRef = useRef<HTMLDivElement>(null);
+    const navBarCollapseButtonRef = useRef<HTMLButtonElement>(null);
     return (
         <nav className="NavBarComponent navbar navbar-expand-lg navbar-light">
             <div className="container">
@@ -14,13 +17,18 @@ export function NavBar() {
                     aria-controls="navbarSupportedContent" 
                     aria-expanded="false" 
                     aria-label="Toggle navigation"
+                    ref={navBarCollapseButtonRef}
                 ><span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" ref={navBarSelectedItemRef}>
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-3 gap-md-4 gap-lg-5">
                     {getListItems().map((item) => (
-                        <li key={item}>
-                            <a className="nav-link NavBarItem" aria-current="page" href="#" >{item}</a>
+                        <li className="nav-item" key={item}>
+                            <a className="nav-link NavBarItem" href={`#${item}`} onClick={() => {
+                                if(navBarSelectedItemRef.current?.classList.contains("show")){
+                                    navBarCollapseButtonRef.current?.click();
+                                }
+                            }}>{item}</a>
                         </li>
                     ))}
                 </ul>
